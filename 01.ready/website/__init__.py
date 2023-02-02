d8a8d90cb7fc46d1786d210bab0c3d5fa3a3be54
angdong <yod1073@gmail.com>
2023-02-02 22:25:49 +0900

    Feat: `ch08`

--- 01.ready/website/__init__.py
+++ 01.ready/website/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'semicircle_secret_key hollimoly guacomole roly poly'
+    app.config['SECRET_KEY'] = 'semicircle_secret_key'
+    app.config["SQLALCHEMY_DATABASE_URI"] = f'splite:///{DB_NAME}'
+    db.init_app(app)
     
     # 블루프린트 인스턴스 가져오기
     from .views import views
